85842f50317b0616f9d65226a511ce485b959575
hyeongwon.lee <hyeongwon.lee@bespinglobal.com>
2024-04-04 14:21:23 +0900

    init

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import requests
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
 @app.get("/")
 async def root():
     return {"message": "Fast API Sample"}
