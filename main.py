fb1a902815def6198b69c65d5b6dc11e741773b3
hyeongwon.lee <hyeongwon.lee@bespinglobal.com>
2024-04-08 15:10:51 +0900

    update deploy-to-ec2.yml

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Fast API Sample & EC2 TEST"}
+    return {"message": "Fast API Sample & EC2 Test..."}
 
 
 @app.get("/weather")
