9939e1f13913536427d04b50370b3aca38126257
hwlee <yosigoon@gmail.com>
2024-05-14 16:06:14 +0900

    rollback

--- app/main.py
+++ app/main.py
@@ -6,44 +6,42 @@ import requests
 import boto3
 import time
 
-logging.basicConfig(level=logging.DEBUG)
-
 app = FastAPI()
 
-# CloudWatch Logs 클라이언트 생성
-logs = boto3.client('logs')
-
-# 로그 그룹 생성
-log_group_name = '/aws/devops/saas/weather-log'
-logs.create_log_group(logGroupName=log_group_name)
-
-# 로그 스트림 생성
-log_stream_name = 'saas-weather-app-stream'
-logs.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
-
-# 로그 이벤트 전송
-log_events = [
-    {
-        'timestamp': int(time.time() * 1000),
-        'message': 'This is a log message.'
-    }
-]
-logs.put_log_events(
-    logGroupName=log_group_name,
-    logStreamName=log_stream_name,
-    logEvents=log_events
-)
+# # CloudWatch Logs 클라이언트 생성
+# logs = boto3.client('logs')
+#
+# # 로그 그룹 생성
+# log_group_name = '/aws/devops/saas/weather-log'
+# logs.create_log_group(logGroupName=log_group_name)
+#
+# # 로그 스트림 생성
+# log_stream_name = 'saas-weather-app-stream'
+# logs.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
+#
+# # 로그 이벤트 전송
+# log_events = [
+#     {
+#         'timestamp': int(time.time() * 1000),
+#         'message': 'This is a log message.'
+#     }
+# ]
+# logs.put_log_events(
+#     logGroupName=log_group_name,
+#     logStreamName=log_stream_name,
+#     logEvents=log_events
+#)
 
 @app.get("/")
 async def root():
-    logging.info("root")
+    #logs.info("root")
     return {"message": "EC2 Fast API Test"}
 
 
 @app.get("/weather")
 async def weather(location: str):
-    logging.info("[GET] API: /weather")
-    logging.info("location: {{}",location)
+    #logs.info("[GET] API: /weather")
+    #logs.info("location: {{}",location)
     # 웹 페이지 가져오기
     html = requests.get('https://search.naver.com/search.naver?query='+location+' 날씨')
 
@@ -59,5 +57,5 @@ async def weather(location: str):
 
 @app.get("/health")
 async def health():
-    logging.info("health check")
+    #logs.info("health check")
     return "ok"
