129cf3456f0a16cef06fd666b93a66be725d500e
hwlee <yosigoon@gmail.com>
2024-05-14 15:31:04 +0900

    rollback

--- app/main.py
+++ app/main.py
@@ -6,23 +6,23 @@ import logging
 
 app = FastAPI()
 # CloudWatch 로그 핸들러 설정
-handler = watchtower.CloudWatchLogHandler(
-    log_group_name='/aws/devops/saas/weather-log',
-    stream_name='saas-weather-app-stream'
-)
-logging.getLogger().addHandler(handler)
-logging.getLogger().setLevel(logging.INFO)
+# handler = watchtower.CloudWatchLogHandler(
+#     log_group_name='/aws/devops/saas/weather-log',
+#     stream_name='saas-weather-app-stream'
+# )
+# logging.getLogger().addHandler(handler)
+# logging.getLogger().setLevel(logging.INFO)
 
 @app.get("/")
 async def root():
-    logging.info("root")
+    #logging.info("root")
     return {"message": "EC2 Fast API Test"}
 
 
 @app.get("/weather")
 async def weather(location: str):
-    logging.info("[GET] API: /weather")
-    logging.info("location: {{}",location)
+    #logging.info("[GET] API: /weather")
+    #logging.info("location: {{}",location)
     # 웹 페이지 가져오기
     html = requests.get('https://search.naver.com/search.naver?query='+location+' 날씨')
 
@@ -38,5 +38,5 @@ async def weather(location: str):
 
 @app.get("/health")
 async def health():
-    logging.info("health check")
+    #logging.info("health check")
     return "ok"
