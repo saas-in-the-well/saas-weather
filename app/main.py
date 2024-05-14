4d11d7423d2f9c5c7052aa646131fbe5c1c65901
hwlee <yosigoon@gmail.com>
2024-05-14 15:59:50 +0900

    log update

--- app/main.py
+++ app/main.py
@@ -1,28 +1,49 @@
+import logging
+
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
-import watchtower
-import logging
+import boto3
+import time
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
-# CloudWatch 로그 핸들러 설정
-# handler = watchtower.CloudWatchLogHandler(
-#     log_group_name='/aws/devops/saas/weather-log',
-#     stream_name='saas-weather-app-stream'
-# )
-# logging.getLogger().addHandler(handler)
-# logging.getLogger().setLevel(logging.INFO)
+
+# CloudWatch Logs 클라이언트 생성
+logs = boto3.client('logs')
+
+# 로그 그룹 생성
+log_group_name = '/aws/devops/saas/weather-log'
+logs.create_log_group(logGroupName=log_group_name)
+
+# 로그 스트림 생성
+log_stream_name = 'saas-weather-app-stream'
+logs.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
+
+# 로그 이벤트 전송
+log_events = [
+    {
+        'timestamp': int(time.time() * 1000),
+        'message': 'This is a log message.'
+    }
+]
+logs.put_log_events(
+    logGroupName=log_group_name,
+    logStreamName=log_stream_name,
+    logEvents=log_events
+)
 
 @app.get("/")
 async def root():
-    #logging.info("root")
+    logging.info("root")
     return {"message": "EC2 Fast API Test"}
 
 
 @app.get("/weather")
 async def weather(location: str):
-    #logging.info("[GET] API: /weather")
-    #logging.info("location: {{}",location)
+    logging.info("[GET] API: /weather")
+    logging.info("location: {{}",location)
     # 웹 페이지 가져오기
     html = requests.get('https://search.naver.com/search.naver?query='+location+' 날씨')
 
@@ -38,5 +59,5 @@ async def weather(location: str):
 
 @app.get("/health")
 async def health():
-    #logging.info("health check")
+    logging.info("health check")
     return "ok"
