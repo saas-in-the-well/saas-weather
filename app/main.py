15aafea96d4eee7f7cba66a684ea311b3671fd57
hwlee <yosigoon@gmail.com>
2024-05-14 14:53:50 +0900

    log update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
+from aws_lambda_powertools import Logger
 import requests
 
 app = FastAPI()
+log = Logger(service="saas-weather-service")
 
 @app.get("/")
 async def root():
+    log.info("root")
     return {"message": "EC2 Fast API Test"}
 
 
 @app.get("/weather")
 async def weather(location: str):
-
+    log.info("[GET] API: /weather")
+    log.info("location: {{}",location)
     # 웹 페이지 가져오기
     html = requests.get('https://search.naver.com/search.naver?query='+location+' 날씨')
 
@@ -27,4 +31,5 @@ async def weather(location: str):
 
 @app.get("/health")
 async def health():
+    log.info("health check")
     return "ok"
